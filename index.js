32633c6cc1f10d0af433b1f55c2cddee0398db19
LakshmiJalakam <jalakamlakshmiprasanna@gmail.com>
2022-11-24 17:12:46 +0530

    post

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=(2000);
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+app.get("/",(req,res)=>{
+	res.send("waiting")
+})
+app.post("/newData",(req,res)=>{
+	const {name,pass}=req.body;
+	console.log(name,pass)
+	res.send("added successfully")
+})
+app.listen(port,()=>console.log("server is running"))
